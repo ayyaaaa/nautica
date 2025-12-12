fa37ce696e0098710202a5fb9ba1726aaa15276e
ayyaaaa <aliayyaaaa9911@gmail.com>
2025-12-13 02:50:00 +0500

    Reset migrations for fresh deploy

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251212_214338_initial_setup from './20251212_214338_initial_setup';
+import * as migration_20251212_214946_initial_setup from './20251212_214946_initial_setup';
 
 export const migrations = [
   {
-    up: migration_20251212_214338_initial_setup.up,
-    down: migration_20251212_214338_initial_setup.down,
-    name: '20251212_214338_initial_setup'
+    up: migration_20251212_214946_initial_setup.up,
+    down: migration_20251212_214946_initial_setup.down,
+    name: '20251212_214946_initial_setup'
   },
 ];
