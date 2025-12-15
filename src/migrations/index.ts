08521ce7d9c4f0d839ca544d6388e86a365aef30
ayyaaaa <aliayyaaaa9911@gmail.com>
2025-12-15 21:43:17 +0500

    fixed2

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251215_162358_add_current_berth_to_vessels from './20251215_162358_add_current_berth_to_vessels';
+import * as migration_20251215_164303_add_current_berth_to_vessels from './20251215_164303_add_current_berth_to_vessels';
 
 export const migrations = [
   {
-    up: migration_20251215_162358_add_current_berth_to_vessels.up,
-    down: migration_20251215_162358_add_current_berth_to_vessels.down,
-    name: '20251215_162358_add_current_berth_to_vessels'
+    up: migration_20251215_164303_add_current_berth_to_vessels.up,
+    down: migration_20251215_164303_add_current_berth_to_vessels.down,
+    name: '20251215_164303_add_current_berth_to_vessels'
   },
 ];
