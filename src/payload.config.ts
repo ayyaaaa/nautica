57354614f45b4f4b1471f11f1c651c755294d897
ayyaaaa <aliayyaaaa9911@gmail.com>
2026-01-10 15:41:50 +0500

    fixed maybe

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -46,7 +46,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    push: process.env.NODE_ENV === 'development',
+    push: true,
   }),
   sharp,
   plugins: [
