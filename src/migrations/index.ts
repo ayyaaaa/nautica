12d3db1b0a6175299959d1cd64e0a3988b69ab5b
ayyaaaa <aliayyaaaa9911@gmail.com>
2026-01-11 16:52:40 +0500

    why??

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260111_115214_somthing from './20260111_115214_somthing';
+
+export const migrations = [
+  {
+    up: migration_20260111_115214_somthing.up,
+    down: migration_20260111_115214_somthing.down,
+    name: '20260111_115214_somthing'
+  },
+];
