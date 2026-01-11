2f052ca27eab68736f2481c7cc3e6fb1757fb3c7
ayyaaaa <aliayyaaaa9911@gmail.com>
2026-01-11 16:49:25 +0500

    puuushed?

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260111_114549_add_finance_fields_to_vessels from './20260111_114549_add_finance_fields_to_vessels';
-
-export const migrations = [
-  {
-    up: migration_20260111_114549_add_finance_fields_to_vessels.up,
-    down: migration_20260111_114549_add_finance_fields_to_vessels.down,
-    name: '20260111_114549_add_finance_fields_to_vessels'
-  },
-];
